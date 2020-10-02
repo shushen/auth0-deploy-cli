ab9c5db9e7155eb9c216276ffa6cf805d283c108
Shu Shen <shu.shen@auth0.com>
2020-10-02 16:58:21 -0700

    chore: refactor tests

--- test/context/directory/clients.test.js
+++ test/context/directory/clients.test.js
@@ -1,112 +1,73 @@
-import fs from 'fs-extra';
 import { constants } from 'auth0-source-control-extension-tools';
 
-import path from 'path';
-import { expect } from 'chai';
-
-import Context from '../../../src/context/directory';
 import handler from '../../../src/context/directory/handlers/clients';
-import { loadJSON } from '../../../src/utils';
-import { cleanThenMkdir, testDataDir, createDir, mockMgmtClient } from '../../utils';
+import { standardTests } from '../../utils';
 
-
-describe('#directory context clients', () => {
-  it('should process clients', async () => {
-    const files = {
-      [constants.CLIENTS_DIRECTORY]: {
-        'someClient.json': '{ "app_type": @@appType@@, "name": "someClient" }',
-        'someClient2.json': '{ "app_type": @@appType@@, "name": "someClient2" }',
-        'customLoginClient.json': '{ "app_type": @@appType@@, "name": "customLoginClient", "custom_login_page": "./customLoginClient_custom_login_page.html" }',
-        'customLoginClient_custom_login_page.html': 'html code'
-      }
-    };
-
-    const repoDir = path.join(testDataDir, 'directory', 'clients1');
-    createDir(repoDir, files);
-
-    const config = { AUTH0_INPUT_FILE: repoDir, AUTH0_KEYWORD_REPLACE_MAPPINGS: { appType: 'spa' } };
-    const context = new Context(config, mockMgmtClient());
-    await context.load();
-
-    const target = [
+const testSpec = {
+  handler: handler,
+  handlerType: 'clients',
+  handlerDir: constants.CLIENTS_DIRECTORY,
+  env: { AUTH0_KEYWORD_REPLACE_MAPPINGS: { appType: 'spa' } },
+  import: {
+    files: {
+      'someClient.json': '{ "app_type": @@appType@@, "name": "someClient" }',
+      'someClient2.json': '{ "app_type": @@appType@@, "name": "someClient2" }',
+      'customLoginClient.json': '{ "app_type": @@appType@@, "name": "customLoginClient", "custom_login_page": "./customLoginClient_custom_login_page.html" }',
+      'customLoginClient_custom_login_page.html': 'html code'
+    },
+    expected: [
       { app_type: 'spa', name: 'customLoginClient', custom_login_page: 'html code' },
       { app_type: 'spa', name: 'someClient' },
       { app_type: 'spa', name: 'someClient2' }
-    ];
-    expect(context.assets.clients).to.deep.equal(target);
-  });
-
-  it('should ignore unknown file', async () => {
-    const files = {
-      [constants.CLIENTS_DIRECTORY]: {
-        'someClient.json': '{ "app_type": @@appType@@, "name": "someClient" }',
-        'README.md': 'something'
-      }
-    };
-
-    const repoDir = path.join(testDataDir, 'directory', 'clients2');
-    createDir(repoDir, files);
-
-    const config = { AUTH0_INPUT_FILE: repoDir, AUTH0_KEYWORD_REPLACE_MAPPINGS: { appType: 'spa' } };
-    const context = new Context(config, mockMgmtClient());
-    await context.load();
-
-    const target = [
-      { app_type: 'spa', name: 'someClient' }
-    ];
-
-    expect(context.assets.clients).to.deep.equal(target);
-  });
-
-  it('should ignore bad clients directory', async () => {
-    const repoDir = path.join(testDataDir, 'directory', 'clients3');
-    cleanThenMkdir(repoDir);
-    const dir = path.join(repoDir, constants.CLIENTS_DIRECTORY);
-    fs.writeFileSync(dir, 'junk');
-
-    const config = { AUTH0_INPUT_FILE: repoDir };
-    const context = new Context(config, mockMgmtClient());
-
-    const errorMessage = `Expected ${dir} to be a folder but got a file?`;
-    await expect(context.load())
-      .to.be.eventually.rejectedWith(Error)
-      .and.have.property('message', errorMessage);
-  });
-
-  it('should dump clients', async () => {
-    const dir = path.join(testDataDir, 'directory', 'clientsDump');
-    cleanThenMkdir(dir);
-    const context = new Context({ AUTH0_INPUT_FILE: dir }, mockMgmtClient());
-
-    context.assets.clients = [
+    ]
+  },
+  export: {
+    json: [
       { app_type: 'spa', name: 'someClient' },
       { app_type: 'spa', name: 'someClient2' },
-      { app_type: 'spa', name: 'customLoginClient', custom_login_page: 'html code' }
-    ];
-
-    const customLoginClientTarget = { app_type: 'spa', name: 'customLoginClient', custom_login_page: './customLoginClient_custom_login_page.html' };
-
-    await handler.dump(context);
-    const clientFolder = path.join(dir, constants.CLIENTS_DIRECTORY);
-    expect(loadJSON(path.join(clientFolder, 'someClient.json'))).to.deep.equal(context.assets.clients[0]);
-    expect(loadJSON(path.join(clientFolder, 'someClient2.json'))).to.deep.equal(context.assets.clients[1]);
-    expect(loadJSON(path.join(clientFolder, 'customLoginClient.json'))).to.deep.equal(customLoginClientTarget);
-    expect(fs.readFileSync(path.join(clientFolder, 'customLoginClient_custom_login_page.html'), 'utf8')).to.equal('html code');
-  });
-
-  it('should dump clients sanitized', async () => {
-    const dir = path.join(testDataDir, 'directory', 'clientsDump');
-    cleanThenMkdir(dir);
-    const context = new Context({ AUTH0_INPUT_FILE: dir }, mockMgmtClient());
-
-    context.assets.clients = [
       { app_type: 'spa', name: 'someClient-test' },
-      { app_type: 'spa', name: 'someClient2/aa' }
-    ];
+      { app_type: 'spa', name: 'someClient2/aa' },
+      { app_type: 'spa', name: 'customLoginClient', custom_login_page: 'html code' }
+    ],
+    expected: [
+      {
+        fileName: 'someClient.json',
+        contentType: 'json',
+        content: { app_type: 'spa', name: 'someClient' }
+      },
+      {
+        fileName: 'someClient2.json',
+        contentType: 'json',
+        content: { app_type: 'spa', name: 'someClient2' }
+      },
+      {
+        fileName: 'someClient-test.json',
+        contentType: 'json',
+        content: { app_type: 'spa', name: 'someClient-test' }
+      },
+      {
+        fileName: 'someClient2-aa.json',
+        contentType: 'json',
+        content: { app_type: 'spa', name: 'someClient2/aa' }
+      },
+      {
+        fileName: 'customLoginClient.json',
+        contentType: 'json',
+        content: { app_type: 'spa', name: 'customLoginClient', custom_login_page: './customLoginClient_custom_login_page.html' }
+      },
+      {
+        fileName: 'customLoginClient_custom_login_page.html',
+        contentType: 'html',
+        content: 'html code'
+      }
+    ]
+  }
+};
 
-    await handler.dump(context);
-    const clientFolder = path.join(dir, constants.CLIENTS_DIRECTORY);
-    expect(loadJSON(path.join(clientFolder, 'someClient-test.json'))).to.deep.equal(context.assets.clients[0]);
-    expect(loadJSON(path.join(clientFolder, 'someClient2-aa.json'))).to.deep.equal(context.assets.clients[1]);
+describe('#directory context clients', () => {
+  Object.keys(standardTests).forEach((test) => {
+    it(test, async () => {
+      await standardTests[test](testSpec);
+    });
   });
 });
--- test/utils.js
+++ test/utils.js
@@ -2,9 +2,11 @@ import fs from 'fs-extra';
 import rmdirSync from 'rmdir-sync';
 import path from 'path';
 import mkdirp from 'mkdirp';
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import log from '../src/logger';
+import dirContext from '../src/context/directory';
+import { loadJSON } from '../src/utils';
 
 log.transports.console.level = 'debug';
 
@@ -114,3 +116,80 @@ export function createDir(repoDir, files) {
     });
   });
 }
+
+async function caseValidateImport(spec) {
+  const files = { [spec.handlerDir]: spec.import.files };
+  const repoDir = fs.mkdtempSync(path.join(testDataDir, 'directory-'));
+  createDir(repoDir, files);
+
+  const config = { AUTH0_INPUT_FILE: repoDir, ...spec.env };
+  const context = new dirContext(config, mockMgmtClient());
+  await context.load();
+  rmdirSync(repoDir);
+
+  expect(context.assets[spec.handlerType]).to.deep.equal(spec.import.expected);
+}
+
+async function caseValidateImportIgnoreUnknown(spec) {
+  const extraFileContents = {
+    'README.md': 'something'
+  };
+  await caseValidateImport({
+    ...spec,
+    ...{
+      import: { ...spec.import, files: { ...spec.import.files, ...extraFileContents } }
+    }
+  });
+}
+
+async function caseValidateImportIgnoreNonDirectoryInput(spec) {
+  const repoDir = fs.mkdtempSync(path.join(testDataDir, 'directory-'));
+  cleanThenMkdir(repoDir);
+  const dir = path.join(repoDir, spec.handlerDir);
+  fs.writeFileSync(dir, 'junk');
+
+  const config = { AUTH0_INPUT_FILE: repoDir, ...spec.env };
+  const context = new dirContext(config, mockMgmtClient());
+  const errorMessage = `Expected ${dir} to be a folder but got a file?`;
+  await expect(context.load())
+    .to.be.eventually.rejectedWith(Error)
+    .and.have.property('message', errorMessage);
+
+  rmdirSync(repoDir);
+}
+
+async function caseValidateExport(spec) {
+  const dir = fs.mkdtempSync(path.join(testDataDir, 'directory-'));
+  const context = new dirContext({ AUTH0_INPUT_FILE: dir }, mockMgmtClient());
+  const typeFolder = path.join(dir, spec.handlerDir);
+  context.assets[spec.handlerType] = spec.export.json;
+  await spec.handler.dump(context);
+
+  spec.export.expected.forEach((f) => {
+    if (f.fileName === null) {
+      expect(fs.pathExistsSync(typeFolder)).to.be.false;
+    } else if (f.contentType === 'json') {
+      expect(loadJSON(path.join(typeFolder, f.fileName))).to.deep.equal(f.content);
+    } else {
+      expect(fs.readFileSync(path.join(typeFolder, f.fileName), 'utf8')).to.deep.equal(f.content);
+    }
+  });
+  rmdirSync(dir);
+}
+
+async function caseValidateExportUndefined(spec) {
+  await caseValidateExport({
+    ...spec,
+    ...{
+      export: { expected: [ { fileName: null } ] }
+    }
+  });
+}
+
+export const standardTests = {
+  'should process import': caseValidateImport,
+  'should ignore unknown file': caseValidateImportIgnoreUnknown,
+  'should ingore non-directory input': caseValidateImportIgnoreNonDirectoryInput,
+  'should export with snaitized file name': caseValidateExport,
+  'should skip undefined assets': caseValidateExportUndefined
+};
